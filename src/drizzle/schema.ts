59aca9880782a28b58741638861da4a47602e949
Parvej Ali <Parvej.ali.dev@gmail.com>
2025-11-11 21:41:19 +0530

    Add projects table and relations in the database schema
    - Introduced a new 'projects' table with fields for id, name, description, userId, createdAt, and updatedAt.
    - Established foreign key relationship between 'projects' and 'users' tables.
    - Updated schema.ts to include the new 'projects' table and its relations.
    - Added migration SQL for creating the 'projects' table and its constraints.
    - Updated package.json to include a new script for running the development server with the studio.

--- src/drizzle/schema.ts
+++ src/drizzle/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm";
 import { pgTable, uuid, timestamp, varchar } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
@@ -8,3 +9,25 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at").notNull().defaultNow(),
 });
+
+export const projects = pgTable("projects", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  name: varchar("name", { length: 255 }).notNull(),
+  description: varchar("description", { length: 255 }),
+  userId: uuid("user_id")
+    .references(() => users.id, { onDelete: "cascade" })
+    .notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  projects: many(projects),
+}));
+
+export const projectsRelations = relations(projects, ({ one }) => ({
+  user: one(users, {
+    fields: [projects.userId],
+    references: [users.id],
+  }),
+}));
